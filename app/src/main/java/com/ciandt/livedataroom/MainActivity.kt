45ae6331e5ff5bc15a9831b9c1bb37ed9c671530
Fabiano França <ffranca@ciandt.com>
2018-06-27 11:24:31 +0000

    Config project dependencies

--- app/src/main/java/com/ciandt/livedataroom/MainActivity.kt
+++ app/src/main/java/com/ciandt/livedataroom/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.ciandt.livedataroom
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/ciandt/livedataroom/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.ciandt.livedataroom
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
