78682b16c452fa089d84f56fa161af33e1f18ce9
Fabiano França <ffranca@ciandt.com>
2018-07-04 19:43:44 +0000

    Create sample of paging with livedata

--- app/src/main/java/com/ciandt/livedataroom/MainActivity.kt
+++ app/src/main/java/com/ciandt/livedataroom/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.ciandt.livedataroom
 
+import android.annotation.SuppressLint
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        viewModel.list.observe(
+            this,
+            Observer { it?.let { listSize.text = "List size: ${it.size}" } })
+
+        viewModel.page.observe(
+            this,
+            Observer { it?.let { pageSize.text = "Page size: ${it.size}" } })
+
+
+        addFive.setOnClickListener {
+            viewModel.addItems(5)
+        }
+
+        addTen.setOnClickListener {
+            viewModel.addItems(10)
+        }
     }
 }
--- app/src/main/java/com/ciandt/livedataroom/MainViewModel.kt
+++ app/src/main/java/com/ciandt/livedataroom/MainViewModel.kt
@@ -1,6 +1,37 @@
 package com.ciandt.livedataroom
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val list = MediatorLiveData<List<Int>>()
+
+    val page: LiveData<List<Int>> = MutableLiveData()
+
+    private val _list = mutableListOf<Int>()
+
+    init {
+        list.addSource(page) {
+            page.value?.let {
+                _list.addAll(it)
+                list.value = _list
+            }
+        }
+
+        (page as MutableLiveData).value = listOf()
+    }
+
+    fun addItems(size: Int) {
+        val list = mutableListOf<Int>()
+
+        for (i in 1..size) {
+            list.add(i)
+        }
+
+        (page as MutableLiveData).value = list
+    }
+
 }
